138115da444595ef817c34a5538e74228fab8efe
Ejas <ejas01@outlook.com>
2023-10-12 01:29:09 +0530

    AppBar method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,30 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset("assets/images/avatar.png"),
-              ),
-            )
-          ],
-        ),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
         child: Text("HOME SCREEN"),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset("assets/images/avatar.png"),
+            ),
+          )
+        ],
       ),
     );
   }
