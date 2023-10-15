e47c1d76488bf58292078d7d25659ac98f508ed8
Ejas <ejas01@outlook.com>
2023-10-15 19:46:39 +0530

    tasks mock

--- lib/widgets/tasks_item.dart
+++ lib/widgets/tasks_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_task/constants/colors.dart';
 
 class tasksItem extends StatelessWidget {
   const tasksItem({super.key});
@@ -6,7 +7,35 @@ class tasksItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Tasks'),
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: tdBlue),
+        title: Text(
+          'Check mail',
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+          ),
+        ),
+      ),
     );
   }
 }
