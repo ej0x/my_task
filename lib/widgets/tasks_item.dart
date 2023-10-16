c8708339282c709d1a0491bc3e2e6cfa204ff2b7
Ejas <ejas01@outlook.com>
2023-10-16 22:31:06 +0530

    const

--- lib/widgets/tasks_item.dart
+++ lib/widgets/tasks_item.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:my_task/constants/colors.dart';
+import 'package:my_task/models/task.dart';
 
 class tasksItem extends StatelessWidget {
-  const tasksItem({super.key});
+  final tasks todo;
+  const tasksItem({super.key, required this.todo});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 20),
+      margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {},
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
-        leading: Icon(Icons.check_box, color: tdBlue),
+        leading: const Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
         title: Text(
-          'Check mail',
-          style: TextStyle(
+          todo.taskText!,
+          style: const TextStyle(
               fontSize: 16,
               color: tdBlack,
               decoration: TextDecoration.lineThrough),
         ),
         trailing: Container(
-          padding: EdgeInsets.all(0),
-          margin: EdgeInsets.symmetric(vertical: 12),
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 12),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
@@ -36,7 +41,7 @@ class tasksItem extends StatelessWidget {
             onPressed: () {},
             color: Colors.white,
             iconSize: 18,
-            icon: Icon(Icons.delete),
+            icon: const Icon(Icons.delete),
           ),
         ),
       ),
