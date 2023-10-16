3dfd060d34511e53ddd40a7dcc16c88630079b71
Ejas <ejas01@outlook.com>
2023-10-16 20:50:19 +0530

    padding

--- lib/widgets/tasks_item.dart
+++ lib/widgets/tasks_item.dart
@@ -7,11 +7,13 @@ class tasksItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {},
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(Icons.check_box, color: tdBlue),
         title: Text(
@@ -22,6 +24,8 @@ class tasksItem extends StatelessWidget {
               decoration: TextDecoration.lineThrough),
         ),
         trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
